030d74e9e1c9054922fdbdd51bc1b7f848262554
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-03-22 15:43:15 +0000

    Update and rename main.rb to example.rb

--- example.rb
+++ example.rb
@@ -3,18 +3,17 @@ require './robot.rb'
 puts "Hey! I am Daniel. I am a moving robot. Which position do I move to?"
 command = gets.chomp.downcase.split("") 
 
-daniel = Robot.new(x: 0, y: 0) #creating new object for input
+daniel = Robot.new(x: 0, y: 0) # creating new object for input
 
-#reading input to change direction of the robot 
-
+# reading input to change direction of the robot 
 command.each do |move|
   case move 
   when "r"
-    daniel.turn_right
+    daniel.turn_right!
   when "a"
-    daniel.move
+    daniel.move!
   when "l"
-    daniel.turn_left
+    daniel.turn_left!
   else 
   end
 end
